0c5949a369b3c577c5fa2c3ffec6dcd9d777cf01
SAMCOder <sankarsamadhan@gmail.com>
2026-06-22 08:23:05 +0000

    converion

---
+++ basic/convesion.js
@@ -0,0 +1,30 @@
+let   scope ="sam";
+//console.log(scope);
+
+let   see =Number(scope);
+//console.log(typeof (scope))
+//c//onsole.log(typeof see);
+
+//console.log(typeof(see));
+//console.log( see);
+//console.log(see);
+
+let   yep =""
+let check =Boolean (yep);
+//console.log(yep);
+//console.log(check);
+/*
+liek  1->true
+   0->flase
+   "" ->flase
+   "sam" ->true
+
+
+
+*/
+
+
+let number=99;
+let conver=String(number)
+console.log(conver);
+console.log(typeof conver)
