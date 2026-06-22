a65bd06e5737ae7dc0597fd59c0077ddd4b1885d
SAMCOder <sankarsamadhan@gmail.com>
2026-06-22 07:55:30 +0000

    Updated lol.js with console.table

--- basic/lol.js
+++ basic/lol.js
@@ -1,1 +1,16 @@
-console.log("SAM")
+//console.log("SAM")
+
+const sam ="samdhan";
+let lol=123;
+var num=123;
+
+lol=32532;
+//console.log(lol);
+ num=800
+ //console.log(num);
+console.log(sam)
+let id;
+console.table([sam,lol,num,id,ok
+    
+])
+ok="hari"
