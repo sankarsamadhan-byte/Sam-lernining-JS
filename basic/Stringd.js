9816d0901940a119533ee91f804258743b2edfe4
SAMCOder <sankarsamadhan@gmail.com>
2026-06-24 09:42:51 +0000

    akk   work

---
+++ basic/Stringd.js
@@ -0,0 +1,23 @@
+// // const newsam ="     samadhan  "
+
+// // console.log(newsam);
+// // console.log(newsam.trim());
+
+// // const  lol="   12 samdhana RUpanrr r    "
+
+// // console.log(lol.trim());
+
+
+// const  url ="hhrps://samdhand.com/sam%20smadhan"
+
+// //console.log(url.replace('%20','--'))
+// console.log(url.replace('sam%20','lolleringgn'))
+
+
+ //to  print  he  Cuuurncey    values  and      percison  vlaie  ******
+
+ const other =124.7644
+
+ console.log(other.toPrecision(4));
+  const hunder=100000000
+  console.log(hunder.toLocaleString('en-IN'))
---
+++ basic/howwrite.js
@@ -0,0 +1,24 @@
+// const name="sam"
+// //const num =40;
+
+
+// //console.log(`Heelo js my name  is ${name.toUpperCase()} and my   rilll numbr is ${num}`)
+
+// const lol="hari"
+// const num=89
+
+// console.log(`this is   best  pr  * to write  code in  JS ${lol},${num}`)
+
+const game=new  String('samadhan')
+
+// console.log([0]);
+// console.log(game.__proto__);
+
+// console.log(game.charAt(2));
+// console.log(game.indexOf('t'));
+
+const newS=game.substring(0,6)
+console.log(newS);
+
+const new2=game.substring(-8,3)
+console.log(new2)
